b31ebceea3421e581ff3b01a6b006eb87deb1f86
Aaditya <aaditya2200@gmail.com>
2021-07-22 13:50:18 +0530

    defined function for date conversion

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,14 @@
 import string
 import random
+from datetime import datetime
 
 
 def create_id():
     str = ''.join(random.choice(string.ascii_letters) for i in range(7))
     str = str.join(random.choice(string.digits) for i in range(3))
     return str
+
+
+def return_as_datetime_object(date):
+    ipo_closing_date = datetime.strptime(date, '%b %d, %Y')
+    return datetime.today(), ipo_closing_date
