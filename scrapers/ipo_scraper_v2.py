0ca63afb8dfa9cf7f4a11bd8fe58c55a02a0980e
Amol Derick Soans <amoldericksoans@gmail.com>
2021-07-23 14:10:06 +0530

    Added RHP scarper

--- scrapers/ipo_scraper_v2.py
+++ scrapers/ipo_scraper_v2.py
@@ -1,7 +1,9 @@
 
 import random
 import requests
+import re
 from bs4 import BeautifulSoup
+
 
 from core.constants import USER_AGENT_LIST
 #from redis_conf import RedisConf
@@ -24,8 +26,8 @@ class IPOScraperv2:
         html_table = soup.find('table', {
             'class': 'table-fill-1 table-fill font-setting'})
         data = IPOScraperv2.table_data_text(html_table)
-
-        return(data[2:])
+        #we can write logic so that it only takes current and future IPO for reddis as we dont really need older ones? idk
+        return(data[:3])
 
     @staticmethod
     def table_data_text(table):
@@ -45,8 +47,53 @@ class IPOScraperv2:
         
         
 
+    ## LOGIC for going through comapny names and running RHP_scraper will come here.
+    #===========================================================================#
     
     
+    @staticmethod
+    def RHP_scraper(companyName):
+        # Right now only printing string of RHP url #saving document feasibility not studied yet
+        companyName = companyName.replace(" ","-")
+        DRHP_URL = "https://investorzone.in/ipo/" + companyName +"/"
+        page = requests.get(URL, header)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        buttons = soup,findAll('div',{'class' : 'button-link'})
+
+        #link = soup.find('a' ,{'href': 'https://d2un9pqbzgw43g.cloudfront.net/main/*drhp.pdf'})
+
+        #there are two link one for draft RHP and another for RHP to access RHP just go for second item in array , some companies wont have RHP ready thats why i kept this array.
+        links =[]
+
+        for b in buttons:
+            try:
+                a = b.find('a')['href']
+
+            except:
+                continue 
+            else:
+                links.append(a)
+                
+        return links
+
+
+
+
+        
+
+
+
+
+        
+
+       
+
+
+
+
+        
+       
 
 
 
